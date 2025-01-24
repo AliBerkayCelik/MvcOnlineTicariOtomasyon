27f23f7d35036ffd1d7faf0a98f3453494651af4
Ali Berkay Celik <aliberkaycelik@hotmail.com>
2025-01-24 17:43:16 +0300

    Add project files.

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/SatisHareket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+	public class SatisHareket
+	{
+        [Key]
+        public int SatisID { get; set; }
+        public DateTime Tarih { get; set; }
+        public int Adet { get; set; }
+        public decimal Fiyat { get; set; }
+        public decimal ToplamTutar { get; set; }
+        public Urun Uruns { get; set; }
+        public Cariler Carilers { get; set; }
+        public Personel Personels { get; set; }
+
+    }
+}
