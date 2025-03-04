668a48c5e00e5fde75dae21773fd9aa8c58917dd
Ali Berkay Celik <aliberkaycelik@hotmail.com>
2025-03-04 22:35:27 +0300

    Dinamik Faturalar Modülü Tamamlandı.

--- MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -66,5 +66,40 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
 		}
+        public ActionResult Dinamik()
+        {
+            Class3 cs = new Class3();
+            cs.deger1 = c.Faturalars.ToList();
+            cs.deger2 = c.FaturaKalems.ToList();
+            return View(cs);
+        }
+        public ActionResult FaturaKaydet(string FaturaSeriNo, string FaturaSiraNo,DateTime Tarih,string VergiDairesi,string Saat,string TeslimEden,string TeslimAlan, string Toplam, FaturaKalem[] kalem)
+        {
+			Faturalar f = new Faturalar();
+			f.FaturaSeriNo = FaturaSeriNo;
+			f.FaturaSiraNo = FaturaSiraNo;
+			f.Tarih = Tarih;
+			f.VergiDairesi = VergiDairesi;
+			f.Saat = Saat;
+			f.TeslimEden = TeslimEden;
+			f.TeslimAlan = TeslimAlan;
+			f.Toplam = decimal.Parse(Toplam);
+			c.Faturalars.Add(f);
+
+			foreach (var item in kalem)
+			{
+				FaturaKalem fk = new FaturaKalem();
+				fk.Aciklama = item.Aciklama;
+				fk.BirimFiyat = item.BirimFiyat;
+				fk.Faturaid = item.Faturaid;
+				fk.Miktar = item.Miktar;
+				fk.Tutar = item.Tutar;
+				c.FaturaKalems.Add(fk);
+
+			}
+			c.SaveChanges();
+            return Json("İşlem Başarılı",JsonRequestBehavior.AllowGet);
+
+		}
 	}
 }
